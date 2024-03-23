844ea6227a5133fbbae7d3438f9a2ea7d32726a8
HP <shubham20152001@gmail.com>
2024-03-23 17:03:49 +0530

    phase 1

---
+++ schema.js
@@ -0,0 +1,15 @@
+//schema validation for server.
+
+const Joi = require('joi');
+
+module.exports.listingSchema  = Joi.object({
+    listing:Joi.object({
+        title:Joi.string().required(),
+        description:Joi.string().required(),
+        location:Joi.string().required(),
+        country:Joi.string().required(),
+        price:Joi.number().required().min(0),
+        image:Joi.string().allow("",null),
+
+    }).required()
+});
